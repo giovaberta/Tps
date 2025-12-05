5e4f16fb46a7c6ae39814df291f8646a970890e6
giovaberta <giovanni.bertacchini@fermi.mo.it>
2025-12-05 17:08:25 +0100

    Update Es_7.cpp

--- Es_7.cpp
+++ Es_7.cpp
@@ -1,15 +1,10 @@
 #include <Wire.h>
 #include <Arduino.h>
-#include <Adafruit_AHTX0.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_BMP280.h>
-#include <ctime>
 
 #define Pulsante 0 // pin pulsante
 
 #define OLED 0x3C // pin display
-#define BMP 0x77  // pin bmp280
-#define AHT 0x38  // pin aht20
 
 #define OLED_SDA 21 // pin SDA
 #define OLED_SCL 22 // pin SCL
@@ -23,68 +18,48 @@ int sec = 0;
 bool premuto = false;
 hw_timer_t *timer0 = NULL;
 
-
-// Interrup timer0
-void IRAM_ATTR lampeggio0() { 
-  // Incremento i secondi ogni secondo
-  sec++;
-}
-
 // Dichiaro una macchina a stati con 3 stati
 typedef enum Stato{
   S1,S2,S3
 }State;
 State ora = S1;
 
+// Interrupt timer0
+void IRAM_ATTR lampeggio0() {
+  // Incremento i secondi ogni secondo
+  sec++;
+}
+
+// Funzione di interrupt per il pulsante
 void IRAM_ATTR isr() {
   // Se premo il pulsante setto la variabile in true
   premuto = true;
-} 
-// Oggetto display
-Adafruit_SSD1306 display = Adafruit_SSD1306(COL, ROW, &Wire, OLED_RST);
-// Crea un oggetto sensore AHTX0
-Adafruit_AHTX0 aht; 
-// Creo un oggetto sensore BMP280
-Adafruit_BMP280 bmp;
+}
 
-
+Adafruit_SSD1306 display= Adafruit_SSD1306(COL,ROW,&Wire,OLED_RST); // Oggetto che controlla il display
 
 void setup() {
   Serial.begin(115200); // Inizializzo la seriale
   pinMode(Pulsante, INPUT); // Inizializzo il pulsante in input
-  // Inizializzazione del dispaly
+  // Inizializzazione del display
   Wire.begin(OLED_SDA,OLED_SCL);
   while (!display.begin(SSD1306_SWITCHCAPVCC,OLED)){
     printf("display instalization failed");
     }
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0,0);
-  display.display();
-  
-  // Inizializzazione del BMP
-  while(!bmp.begin(BMP)){
-    printf("BMP280 instalization failed");
-  }
-
-  // Inizializzazione del AHT
-  while(!aht.begin(&Wire,AHT)){
-    printf("AHT20 instalization failed");
-  }
-
-
+  display.setTextColor(WHITE); //Colore testo impostato a bianco
+  display.setTextSize(1); // Dimensione del testo impostato a 1
+  display.setCursor(0,0); // Inizializzo il puntatore a coordinate 0,0 ovvero l'angolo in alto a sinistra
+  display.clearDisplay(); // Pulisco il display
+ 
   // Inizializzazione del timer0
   timer0 = timerBegin(0, 80, true);
   timerAttachInterrupt(timer0, &lampeggio0, true);
-  timerAlarmWrite(timer0, 1000000, true); // Attiva interrup ogni 1 secondo 
-  timerAlarmEnable(timer0); 
+  timerAlarmWrite(timer0, 1000000, true); // Attiva interrupt ogni 1 secondo
+  timerAlarmEnable(timer0);
 
-  // Inizializzazione del Interupt
+  // Inizializzazione del Interrupt
   attachInterrupt(Pulsante, isr,FALLING);
-
   }
-
 
 void loop() {
   // Macchina a stati
