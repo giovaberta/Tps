bd732462f66bae125684e7cb836a0c7bce8bac49
giovaberta <giovanni.bertacchini@fermi.mo.it>
2025-12-05 17:04:30 +0100

    Update Es_6.cpp
    update

--- Es_6.cpp
+++ Es_6.cpp
@@ -1,15 +1,16 @@
 #include <Wire.h>
 #include <Adafruit_AHTX0.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_BMP280.h>
 
-#define OLED_SDA 21
-#define OLED_SCL 22
-#define OLED_RST -1
-#define ROW 64
-#define COL 128
+#define OLED 0x3C // pin del display
 
-Adafruit_AHTX0 aht; // Crea un oggetto sensore AHTX0
+#define OLED_SDA 21 // pin SDA
+#define OLED_SCL 22 // pin SCL
+#define OLED_RST -1 // RESET OLED
+#define ROW 64 // numero righe
+#define COL 128 // numero colonne
+
+Adafruit_AHTX0 aht; // Crea un oggetto sensore AHT20
 Adafruit_SSD1306 display= Adafruit_SSD1306(COL,ROW,&Wire,OLED_RST); // Oggetto che controlla il display
 
 void setup()
@@ -17,18 +18,17 @@ void setup()
   Serial.begin(115200);
   Wire.begin(OLED_SDA,OLED_SCL);
   // Se non rileva il display manda in errore
-  while(!display.begin(SSD1306_SWITCHCAPVCC,0x3C)){ 
+  while(!display.begin(SSD1306_SWITCHCAPVCC,0x3C)){
     Serial.printf("Display initialization falied");
   }
   display.setTextColor(WHITE); //Colore testo impostato a bianco
   display.setTextSize(1); // Dimensione del testo impostato a 1
-  display.setCursor(0,0); // Inizzializzo il puntatore a codinate 0,0 ovvero l'angolo in alto a sinistra
+  display.setCursor(0,0); // Inizializzo il puntatore a coordinate 0,0 ovvero l'angolo in alto a sinistra
   display.clearDisplay(); // Pulisco il display
 
   while (!Serial) delay(10); // Attende la connessione della porta seriale
   Serial.println("Test Sensore AHT20");
 
-  
   if (! aht.begin()) //Inizializza il sensore
   {
   Serial.println("Errore: Impossibile trovare il sensore AHT20! Controlla i collegamenti I2C.");
@@ -41,7 +41,7 @@ void loop()
 {
   // Struttura per memorizzare i dati dell'evento
   sensors_event_t miaUmidita, miaTemperatura;
-  // Ottieni i nuovi dati dal sensore
+  // Ottieni i nuovi dati dal sensore da due eventi
   if (aht.getEvent(&miaUmidita, &miaTemperatura)){
     // Stampa i valori letti sulla seriale
     Serial.print("Temperatura: ");
@@ -51,12 +51,13 @@ void loop()
     Serial.print(miaUmidita.relative_humidity);
     Serial.println(" %");
 
+
     // Stampa i valori letti su oled
     display.clearDisplay();
     display.setCursor(0,0);
     display.print("Temperatura: ");
     display.print(miaTemperatura.temperature);
-    dysplay.print("\xF8")
+    display.print("\xF8");
     display.println(" C");
     display.print("Umidità: ");
     display.print(miaUmidita.relative_humidity);
