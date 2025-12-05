40315db6509dfc7007a596968908be421cc0add2
giovaberta <giovanni.bertacchini@fermi.mo.it>
2025-12-05 17:20:30 +0100

    Update Es_10.cpp

--- Es_10.cpp
+++ Es_10.cpp
@@ -1,11 +1,8 @@
 #include <Wire.h>
 #include <Arduino.h>
-#include <Adafruit_AHTX0.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_BMP280.h>
 
 #define OLED 0x3C // pin display
-
 
 #define OLED_SDA 21 // pin SDA
 #define OLED_SCL 22 // pin SCL
@@ -13,31 +10,29 @@
 #define ROW 64 // numero righe
 #define COL 128 // numero colonne
 
-// Oggetto display
-Adafruit_SSD1306 display = Adafruit_SSD1306(COL, ROW, &Wire, OLED_RST);
+Adafruit_SSD1306 display= Adafruit_SSD1306(COL,ROW,&Wire,OLED_RST); // Oggetto che controlla il display
 
 void setup() {
-  Serial.begin(115200); // Inizializzo la seriale
-  // Inizializzazione del dispaly
+  // Inizializzazione del display
   Wire.begin(OLED_SDA,OLED_SCL);
+  // Se non rileva il display manda in errore
   while (!display.begin(SSD1306_SWITCHCAPVCC,OLED)){
     printf("display instalization failed");
     }
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0,0);
-  display.display();
-  
+  display.setTextColor(WHITE); //Colore testo impostato a bianco
+  display.setTextSize(1); // Dimensione del testo impostato a 1
+  display.setCursor(0,0); // Inizializzo il puntatore a coordinate 0,0 ovvero l'angolo in alto a sinistra
+  display.clearDisplay(); // Pulisco il display
+ 
 }
 void loop() {
-    int rip = 40;
-    for(int cont = 0;cont<40;cont+=4){
+    int rip = 40; // Contatore che comincia a 40 e diminuisce 4 alla volta
+    for(int cont = 0;cont<40;cont+=4){ // Contatore che comincia a 0 e aumenta 4 alla volta
       rip = rip - 4;
-      display.clearDisplay();
-      display.drawCircle(64,32,cont,WHITE);
-      display.drawCircle(64,32,rip,WHITE);
+      display.clearDisplay(); // Ripulisco il display
+      display.drawCircle(64,32,cont,WHITE); // Disegno il cerchio che si sta allargando
+      display.drawCircle(64,32,rip,WHITE); // Disegno il cerchio che si sta rimpicciolendo
       display.display();
-      delay(50);
+      delay(50); // Attendo 50ms
     }
 }
